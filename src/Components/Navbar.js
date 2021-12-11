eb5b87500db07eb124f0dc2a09de2e06f530582f
Saurabh05Chauhan <92812918+Saurabh05Chauhan@users.noreply.github.com>
2021-12-11 21:49:12 +0530

    initial commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar(props) {
     return (
         <>
-            <nav className={`navbar navbar-expand-lg navbar navbar-${props.mode} bg-${props.mode}`}>
+            <nav className={`navbar  navbar-expand-lg navbar navbar-${props.mode} bg-${props.mode}`}>
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/">{props.title}</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
